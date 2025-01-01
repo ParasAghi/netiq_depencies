9f24b3b5298ebe58b78444de3de5684d6f8987b3
ParasAghi <51487346+ParasAghi@users.noreply.github.com>
2025-01-02 03:36:27 +0530

    Update idm.sh

--- idm.sh
+++ idm.sh
@@ -33,7 +33,7 @@ echo "Please reboot the system for changes to take effect."
 
 
 
-yum install bc lsof ksh yum-utils createrepo unzip zip net-tools libgcc*.i686 libncurses*  iproute initscripts -y
+yum install bc lsof ksh yum-utils createrepo unzip zip net-tools libgcc*.i686 libncurses* ncurses-libs  iproute initscripts -y
 
 yum install libXtst-*.i686 libXrender-*.i686 libXi-*.i686  glibc-*.i686 libgcc-*.i686 gettext libXau.i686 libxcb.i686 libstdc++ libnsl* libnsl*.i686 libX11.i686 libXext.i686  -y
 
